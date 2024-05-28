83e647c740b6c7113762915c9e4e9e9ad9f5f77e
Kacper <105165506+TakiKacper@users.noreply.github.com>
2024-05-28 21:03:39 +0200

    Update path_operations.hpp

--- source/path_operations.hpp
+++ source/path_operations.hpp
@@ -6,7 +6,7 @@ namespace filesystem_extended
 		is_subdir
 		returns whether given item is located in the dir or deeper
 	*/
-	bool is_subdir(std::filesystem::path item, std::filesystem::path dir)
+	inline bool is_subdir(std::filesystem::path item, std::filesystem::path dir)
 	{
 		if (!std::filesystem::exists(item)) throw std::exception({"Failed to check for the path belongingness; item does not exists"});
 		if (!std::filesystem::exists(dir)) throw std::exception({ "Failed to check for the path belongingness; dir does not exists" });
